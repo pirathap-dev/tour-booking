3c54ad7880546babf03c4e63847d6af36fc47250
pirathap-dev <pirathap.dev@gmail.com>
2025-04-15 11:14:04 +0530

    axios error fixed

--- my-app/src/confiq/axiosConfig.js
+++ my-app/src/confiq/axiosConfig.js
@@ -1,8 +1,22 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//     baseURL: import.meta.env.BACKEND_URL + "/api/v1",
+//     withCredentials: true,
+// });
+
+// export default api;
+
 import axios from "axios";
+import dotenv from "dotenv";
+
+// Load env vars
+dotenv.config();
 
 const api = axios.create({
-    baseURL: import.meta.env.BACKEND_URL + "/api/v1",
+    baseURL: process.env.BACKEND_URL + "/api/v1",
     withCredentials: true,
 });
 
 export default api;
+
