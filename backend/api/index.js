876ba210c594914ee77d132fd467d9ed9029d02b
pirathap-dev <pirathap.dev@gmail.com>
2025-04-15 15:56:04 +0530

    final

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
-const app = require('../app'); // Adjust the path based on your project structure
+const app = require('../app');
 
 module.exports.handler = serverless(app);
