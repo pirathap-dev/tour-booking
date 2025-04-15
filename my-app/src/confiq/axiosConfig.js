7e8ec3b6704cc070137759671c6c1a6eb901ad7e
pirathap-dev <pirathap.dev@gmail.com>
2025-04-15 11:04:03 +0530

    backend url updated

--- my-app/src/confiq/axiosConfig.js
+++ my-app/src/confiq/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8800/api/v1",
+    baseURL: import.meta.env.BACKEND_URL + "/api/v1",
     withCredentials: true,
 });
 
