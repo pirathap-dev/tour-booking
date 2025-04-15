753bf6957d90336826a148005a5fe6c6f6d2aec7
pirathap-dev <pirathap.dev@gmail.com>
2025-04-15 19:14:40 +0530

    vercel Updated code

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,16 +1,16 @@
-const serverless = require('serverless-http');
-const app = require('../app');
-const connectDatabase = require('../config/database');
+// const serverless = require('serverless-http');
+// const app = require('../app');
+// const connectDatabase = require('../config/database');
 
-let handler;
+// let handler;
 
-module.exports = async (req, res) => {
-  try {
-    await connectDatabase(); // make sure DB is ready
-    if (!handler) handler = serverless(app); // only wrap once
-    return handler(req, res);
-  } catch (err) {
-    console.error('Serverless function error:', err);
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+// module.exports = async (req, res) => {
+//   try {
+//     await connectDatabase(); // make sure DB is ready
+//     if (!handler) handler = serverless(app); // only wrap once
+//     return handler(req, res);
+//   } catch (err) {
+//     console.error('Serverless function error:', err);
+//     return res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// };
