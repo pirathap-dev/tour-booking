dbbd27cf5a06ec1eed5542a67b63663240e5093c
pirathap-dev <pirathap.dev@gmail.com>
2025-04-15 14:22:36 +0530

    cors 3

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const corsOptions = {
 };
 
 app.use(express.json());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
