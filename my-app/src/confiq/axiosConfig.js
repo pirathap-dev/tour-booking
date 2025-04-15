afaef4f58578f2c60055c7cb7b4b685c64bb4f5c
pirathap-dev <pirathap.dev@gmail.com>
2025-04-15 11:44:41 +0530

    env fixed

--- my-app/src/confiq/axiosConfig.js
+++ my-app/src/confiq/axiosConfig.js
@@ -1,12 +1,3 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//     baseURL: import.meta.env.BACKEND_URL + "/api/v1",
-//     withCredentials: true,
-// });
-
-// export default api;
-
 import axios from "axios";
 
 const api = axios.create({
