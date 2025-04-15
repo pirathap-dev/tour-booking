ae5ae307c7c54992500daf32daae382a0ddb7035
pirathap-dev <pirathap.dev@gmail.com>
2025-04-15 13:10:10 +0530

    jwt request error fixed

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -5,7 +5,9 @@ const sendToken = (user, statusCode, res) => {
     //Setting cookies
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
-        httpOnly: true
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",  // Required for Vercel
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     }
 
     res.status(statusCode).cookie('token', token, options).json({
